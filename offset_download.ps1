49ad2441edcfb11d3fbcc1b2fb5a647d8c8f0739
kristofhracza <kristofhracza@gmail.com>
2024-04-03 18:23:01 +0100

    Adjusted url for offsets

--- offset_download.ps1
+++ offset_download.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/a2x/cs2-dumper/main/generated/offsets.json -OutFile offsets.json
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/a2x/cs2-dumper/main/generated/client.dll.json -OutFile client.dll.json
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.json -OutFile offsets.json
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client.dll.json -OutFile client.dll.json
